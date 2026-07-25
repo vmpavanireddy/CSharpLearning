ec144d8556aa05d5956539dbfd8af18038f7d410
vmpavanireddy <vmpavanireddy@gmail.com>
2026-07-25 22:05:42 +0530

    change the value of a variable

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,3 +8,18 @@ Console.WriteLine("yo, " + Name + "!");
 //= - assignment operator (stores the value)
 //value - value assigned to the variable
 //;-semicolon (end of statement)
+
+//changing the value of a variable
+Name = "deepthi";
+Console.WriteLine("hlo, " + Name + "!");
+//The second time we didn't write string again.
+//This is because the variable was already created.
+//We can change the value of a variable as many times as we want.
+string Name1 = "pavani";
+Console.WriteLine("hii, " + Name1 + "!");
+int age = 20;
+Console.WriteLine("age is: " + age);
+bool loggedIn = true;
+Console.WriteLine("loggedIn: " + loggedIn);
+double salary = 200000.5;
+Console.WriteLine("salary is: " + salary);
