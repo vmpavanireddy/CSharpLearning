278ac2305f42920cef4d10b51834ae2605409745
vmpavanireddy <vmpavanireddy@gmail.com>
2026-07-25 22:05:17 +0530

    variable syntax and declaration

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,10 @@
 ﻿Console.WriteLine("Hello, pavani!");
+//variable syntax
+//datatype variableName = value;
+string Name = "divya";
+Console.WriteLine("yo, " + Name + "!");
+//string -data type
+//name-variableName 
+//= - assignment operator (stores the value)
+//value - value assigned to the variable
+//;-semicolon (end of statement)
