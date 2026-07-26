5a8239e5b2ebb70b7ad0f485c33ecab19ce1f3f4
vmpavanireddy <vmpavanireddy@gmail.com>
2026-07-26 23:32:57 +0530

    switch statement

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -207,9 +207,41 @@ if (weekend || holiday)
 {
     Console.WriteLine("No office today!");
 }
-bool isLoggedIn = false;
+bool isLogged_In = false;
 
-if (!isLoggedIn)
+if (!isLogged_In)
 {
     Console.WriteLine("Please login.");
+}
+//switch statement
+int day = 3;
+
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+        case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+        break;  
+        case 6:
+        Console.WriteLine("Saturday");
+        break;
+        case 7:
+        Console.WriteLine("Sunday");
+        break;
+    default:
+        Console.WriteLine("Invalid Day");
+        break;
 }
