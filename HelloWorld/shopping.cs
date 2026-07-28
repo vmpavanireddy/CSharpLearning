e15560a0d406489808c84a1b4cbb4863b6b28e3a
vmpavanireddy <vmpavanireddy@gmail.com>
2026-07-28 11:27:32 +0530

    cart

--- HelloWorld/shopping.cs
+++ HelloWorld/shopping.cs
@@ -1,8 +1,19 @@
 using System;
-public class shopping
+public class Shopping
 {
     public void cart()
     {
-        
+
+        int org = 250;
+        int disc = 0;
+        int final = 0;
+        if (org > 200)
+        {
+            disc = 20;
+        }
+        final = org - disc;
+        Console.WriteLine("original price: " + org);
+        Console.WriteLine("discount: " + disc);
+        Console.WriteLine("final price: " + final);
     }
 }
