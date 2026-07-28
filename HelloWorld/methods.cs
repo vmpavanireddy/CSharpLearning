aca12e8edb5a2951a396858ef7d2293f2e5726a1
vmpavanireddy <vmpavanireddy@gmail.com>
2026-07-28 23:19:33 +0530

    method with return value

--- HelloWorld/methods.cs
+++ HelloWorld/methods.cs
@@ -9,6 +9,9 @@ public class Methods
         Greet("pavani");
         //method with multiple parameters
         Student("divya", 19);
+        int result = Add(10, 20);
+        Console.WriteLine(result);
+        StartProgram();
     }
     static void PrintWelcome()
     {
@@ -18,10 +21,28 @@ public class Methods
     {
         Console.WriteLine($"Hello {name}");
     }
-     static void Student(string nam, int age)
+    static void Student(string nam, int age)
     {
         Console.WriteLine($"Name : {nam}");
         Console.WriteLine($"Age : {age}");
     }
-    
+    //Returning Values-Send the answer back to the place where the method was called.
+    //Every int method must return an int.
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+    //Methods Can Call Other Methods
+    static void StartProgram()
+    {
+        Console.WriteLine("Starting...");
+
+        ShowMenu();
+    }
+
+    static void ShowMenu()
+    {
+        Console.WriteLine("1. Login");
+        Console.WriteLine("2. Exit");
+    }
 }
