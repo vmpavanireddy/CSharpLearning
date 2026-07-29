38658d307cef6217c38ae71309413edd984b0864
vmpavanireddy <vmpavanireddy@gmail.com>
2026-07-29 22:14:31 +0530

    Constructor with Parameters

--- HelloWorld/clg.cs
+++ HelloWorld/clg.cs
@@ -1,9 +1,20 @@
 using System;
 public class student
 {
-    public Student()
+    public student()
     {
         Console.WriteLine("Student object created.");
+    }
+}
+class Students_no
+{
+    public string Name;
+    public int Age;
+
+    public Students_no(string name, int age)
+    {
+        Name = name;
+        Age = age;
     }
 }
 public class clg
@@ -12,9 +23,11 @@ public class clg
     {
         //A constructor is a special method that runs automatically when an object is created.
         //There is no return type.Not even void.
-        student regno=new student();
+        student regno = new student();
         //We never wrote:student.Student();The constructor runs automatically.
-        
-        
+        Students_no stu_no = new Students_no("deepthi", 18);
+        Console.WriteLine(stu_no.Name);
+        Console.WriteLine(stu_no.Age);
+
     }
 }
