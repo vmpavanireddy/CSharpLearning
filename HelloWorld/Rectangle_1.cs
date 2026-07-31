b505ccf3ab95418a0bcb88294ff653a9d03e122c
vmpavanireddy <vmpavanireddy@gmail.com>
2026-07-31 23:50:25 +0530

    example

---
+++ HelloWorld/Rectangle_1.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Rectangle_1
+{
+    public int Length { get; set; }
+    public int Width { get; set; }
+
+    public int Area => Length * Width;
+}
--- HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c63e2369a629f3781c94c8365db4856a5ae45d89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c916ae25d899a42f984c7e1319a5c788e7eed38f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
