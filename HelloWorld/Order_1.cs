541ebac4758caad39582d5613a523d7cd991a8e8
vmpavanireddy <vmpavanireddy@gmail.com>
2026-07-31 23:50:50 +0530

    example

---
+++ HelloWorld/Order_1.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Order_1
+{
+    public int OrderId { get; private set; }
+
+    public Order_1(int id)
+    {
+        OrderId = id;
+    }
+}
--- HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c916ae25d899a42f984c7e1319a5c788e7eed38f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b505ccf3ab95418a0bcb88294ff653a9d03e122c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
