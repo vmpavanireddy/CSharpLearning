c3e66a8173c783b7512c934da08da5c13b369745
vmpavanireddy <vmpavanireddy@gmail.com>
2026-08-01 20:59:19 +0530

    example

--- HelloWorld/Order_1.cs
+++ HelloWorld/Order_1.cs
@@ -3,9 +3,19 @@ using System;
 class Order_1
 {
     public int OrderId { get; private set; }
+    private double balance;
+
+public double Balance
+{
+    get
+    {
+        return balance;
+    }
+}
 
     public Order_1(int id)
     {
         OrderId = id;
+        balance = 0.0;
     }
 }
--- HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b505ccf3ab95418a0bcb88294ff653a9d03e122c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+541ebac4758caad39582d5613a523d7cd991a8e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
