c22e9d1992a91b9be5af8e6ff50f2df14421db19
vmpavanireddy <vmpavanireddy@gmail.com>
2026-08-07 23:43:55 +0530

    inheritance using protected member

--- HelloWorld/Program_7.cs
+++ HelloWorld/Program_7.cs
@@ -5,12 +5,15 @@ class Animal_2
     private int age;
 
     public string Name { get; set; }
+    //if we use protected
+     protected int age_animal;
 }
 class Dog_1 : Animal_2
 {
     public void Test()
     {
         // age = 5; ❌//error //The Dog class cannot directly access Animal's private field.Even though Dog inherits from Animal.
+        age_animal = 5; // ✅//This is allowed because age_animal is protected and Dog inherits from Animal.
     }
 }
 class Program_7
