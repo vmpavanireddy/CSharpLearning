b9eb19a2261a64564fa2718776aea57add9d6744
vmpavanireddy <vmpavanireddy@gmail.com>
2026-08-08 23:41:19 +0530

    polymorphism define

---
+++ HelloWorld/obj/Debug/net10.0/program_12.cs
@@ -0,0 +1,28 @@
+using System;
+//polymorphism
+//The same method call can behave differently depending on the object.
+/*
+Imagine you tell:
+
+"Make a sound!"
+
+Different animals respond differently:
+
+Dog → Woof!
+Cat → Meow!
+Cow → Moo!
+
+The command is the same:
+
+MakeSound()
+
+But the behavior is different.
+
+That's polymorphism.
+*/
+class program_12
+{
+    static void Main(string[] args)
+    {
+    }
+}
