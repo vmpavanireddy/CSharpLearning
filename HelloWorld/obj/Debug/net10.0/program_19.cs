fe7497efc7744cda9de29e460c916f76bf8b4f95
vmpavanireddy <vmpavanireddy@gmail.com>
2026-08-09 23:49:33 +0530

    example class

---
+++ HelloWorld/obj/Debug/net10.0/program_19.cs
@@ -0,0 +1,15 @@
+//example
+using System;
+abstract class Character
+{
+    public string Name { get; set; }
+
+    public abstract void Attack();
+}
+class program_19
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
