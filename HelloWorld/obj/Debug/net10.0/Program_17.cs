58e4271d944544fa983ded01be31fd901acafc72
vmpavanireddy <vmpavanireddy@gmail.com>
2026-08-09 23:46:16 +0530

    example

--- HelloWorld/obj/Debug/net10.0/Program_17.cs
+++ HelloWorld/obj/Debug/net10.0/Program_17.cs
@@ -20,6 +20,13 @@ class CreditCardPayment : Payment
         Console.WriteLine("Payment made using Credit Card");
     }
 }
+class CashPayment : Payment
+{
+    public override void MakePayment()
+    {
+        Console.WriteLine("Payment made using Cash");
+    }
+}
 class Program_17
 {
     static void Main()
