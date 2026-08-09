1abc559904f7a347ec75f5516df9dd613ad074d0
vmpavanireddy <vmpavanireddy@gmail.com>
2026-08-09 23:45:48 +0530

    Abstraction

---
+++ HelloWorld/obj/Debug/net10.0/Program_17.cs
@@ -0,0 +1,25 @@
+//Abstraction in C#
+using System;
+
+abstract class Payment
+{
+    public abstract void MakePayment();
+}
+
+class UpiPayment : Payment
+{
+    public override void MakePayment()
+    {
+        Console.WriteLine("Payment made using UPI");
+    }
+}
+
+class Program_17
+{
+    static void Main()
+    {
+        UpiPayment payment = new UpiPayment();
+
+        payment.MakePayment();
+    }
+}
