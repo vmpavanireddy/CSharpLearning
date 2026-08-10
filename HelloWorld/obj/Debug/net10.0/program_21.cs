3688ffa5412586cd9abc08c3ff28b65b68506359
vmpavanireddy <vmpavanireddy@gmail.com>
2026-08-10 23:45:31 +0530

    main

--- HelloWorld/obj/Debug/net10.0/program_21.cs
+++ HelloWorld/obj/Debug/net10.0/program_21.cs
@@ -5,10 +5,25 @@ interface IPayment
     void MakePayment();
     void Refund();
 }
+class UpiPayment : IPayment
+{
+    public void MakePayment()
+    {
+        Console.WriteLine("UPI payment");
+    }
+
+    public void Refund()
+    {
+        Console.WriteLine("UPI refund");
+    }
+}
 class program_21
 {
     static void Main(string[] args)
     {
+        UpiPayment payment = new UpiPayment();
+        payment.MakePayment();
+        payment.Refund();
 
     }
 }
