2013e7cd21e92d1cd5f1ca7f983178021264b168
vmpavanireddy <vmpavanireddy@gmail.com>
2026-08-10 23:39:43 +0530

    syntax of interface

--- HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+443973ae9d5cb1ff4c82f407b20e781153d0415f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53cacaf184901c96cb2806c4ae7553d528693bc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ HelloWorld/obj/Debug/net10.0/Program_20.cs
@@ -0,0 +1,20 @@
+//Interfaces in C#
+using System;
+//An interface defines a contract — it tells a class what it must be able to do, without deciding how it does it.
+//syntax of interface
+/*
+interface interface_name
+{
+    //methods
+    //properties
+    //events
+    //indexers
+}
+*/
+class Program_20
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
