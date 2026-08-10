755b7ab75528b3818eca5c7addb3728a90a5e700
vmpavanireddy <vmpavanireddy@gmail.com>
2026-08-10 23:45:00 +0530

    interface example

---
+++ HelloWorld/obj/Debug/net10.0/program_21.cs
@@ -0,0 +1,14 @@
+using System;
+//Interface doesn't normally contain implementation:It's describing what must exist.
+interface IPayment
+{
+    void MakePayment();
+    void Refund();
+}
+class program_21
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
