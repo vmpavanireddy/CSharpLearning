e870b03bd25855a79f20cef889f34474f7c46f98
vmpavanireddy <vmpavanireddy@gmail.com>
2026-08-10 23:46:38 +0530

    Multiple Interfaces

---
+++ HelloWorld/obj/Debug/net10.0/Program_22 .cs
@@ -0,0 +1,30 @@
+//Multiple Interfaces
+using System;
+interface IPrintable
+{
+    void Print();
+}
+
+interface IScannable
+{
+    void Scan();
+}
+class Printer : IPrintable, IScannable
+{
+    public void Print()
+    {
+        Console.WriteLine("Printing...");
+    }
+
+    public void Scan()
+    {
+        Console.WriteLine("Scanning...");
+    }
+}
+class Program_22
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
