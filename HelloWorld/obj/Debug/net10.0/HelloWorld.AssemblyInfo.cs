f84ad121ab6301082fb570f4785cc48f5d5e39e6
vmpavanireddy <vmpavanireddy@gmail.com>
2026-08-11 23:46:28 +0530

    interface iuserserive

--- HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53cacaf184901c96cb2806c4ae7553d528693bc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a92a8cb8f70f842efa0c1f65eb33c5350b04a73a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ HelloWorld/obj/Debug/net10.0/Program_23.cs
@@ -0,0 +1,18 @@
+//Multiple Interfaces
+/*IUserService
+IEmailService
+ILogger
+IPaymentService
+*/
+interface IUserService
+{
+    void CreateUser(string username, string email);
+    void DeleteUser(int userId);
+}
+class Program_23
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
