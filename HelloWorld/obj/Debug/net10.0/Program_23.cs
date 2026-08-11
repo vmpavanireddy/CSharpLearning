94fa53d108bbe7cde1741fb12a229e794604ed1b
vmpavanireddy <vmpavanireddy@gmail.com>
2026-08-11 23:47:07 +0530

    interface iemailservice

--- HelloWorld/obj/Debug/net10.0/Program_23.cs
+++ HelloWorld/obj/Debug/net10.0/Program_23.cs
@@ -9,6 +9,11 @@ interface IUserService
     void CreateUser(string username, string email);
     void DeleteUser(int userId);
 }
+interface IEmailService
+{
+    void SendEmail(string to, string subject, string body);
+}
+
 class Program_23
 {
     static void Main(string[] args)
