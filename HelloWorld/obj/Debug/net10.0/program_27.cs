1f4e0adf430baf33762cb500170d93ddcb351d70
vmpavanireddy <vmpavanireddy@gmail.com>
2026-08-12 21:43:05 +0530

    class Developer

--- HelloWorld/obj/Debug/net10.0/program_27.cs
+++ HelloWorld/obj/Debug/net10.0/program_27.cs
@@ -8,6 +8,17 @@ interface IEmployee
 
     void Work();
 }
+class Developer : IEmployee
+{
+    public string Name { get; set; }
+
+    public double Salary { get; set; }
+
+    public void Work()
+    {
+        Console.WriteLine("Developer is writing code");
+    }
+}
 class program_27
 {
     static void Main(string[] args)
