ab07d9a3bb45bf9cd499af3863295effa879db17
vmpavanireddy <vmpavanireddy@gmail.com>
2026-08-13 22:40:23 +0530

    bool pay

--- HelloWorld/obj/Debug/net10.0/program_28.cs
+++ HelloWorld/obj/Debug/net10.0/program_28.cs
@@ -2,6 +2,8 @@ using System;
 interface IPaymentService
 {
     void ProcessPayment(decimal amount);
+    bool Pay(double amount);
+
 }
 class program_28
 {
@@ -9,5 +11,6 @@ class program_28
     {
         IPaymentService paymentService = new CreditCardPaymentService();
         paymentService.ProcessPayment(100.00m);
+        bool paymentResult = paymentService.Pay(100.00);
     }
 }
