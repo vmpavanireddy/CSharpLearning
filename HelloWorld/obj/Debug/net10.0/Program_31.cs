5130c70e649942478e2ed8f22af30a2f315bfa98
vmpavanireddy <vmpavanireddy@gmail.com>
2026-08-14 23:41:00 +0530

    example

--- HelloWorld/obj/Debug/net10.0/Program_31.cs
+++ HelloWorld/obj/Debug/net10.0/Program_31.cs
@@ -17,6 +17,11 @@ class Program_31
 {
     static void Main(string[] args)
     {
-       
+        Cart cart = new Cart();
+        cart.AddItem(10.99);
+        cart.AddItem(5.49);
+        cart.AddItem(3.75);
+
+        Console.WriteLine("Total: $" + cart.GetTotal());
     }
 }
