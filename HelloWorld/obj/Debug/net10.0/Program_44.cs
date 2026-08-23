4f4d65ce782d5c9159b888285abf30b56d61cc38
vmpavanireddy <vmpavanireddy@gmail.com>
2026-08-23 23:30:51 +0530

    Multiple interface inheritance

---
+++ HelloWorld/obj/Debug/net10.0/Program_44.cs
@@ -0,0 +1,42 @@
+using System;
+interface IMovable
+{
+    void Move();
+}
+
+interface IAttackable
+{
+    void Attack();
+}
+
+interface IPlayer : IMovable, IAttackable
+{
+    void Heal();
+}
+class Player : IPlayer
+{
+    public void Move()
+    {
+        Console.WriteLine("Move");
+    }
+
+    public void Attack()
+    {
+        Console.WriteLine("Attack");
+    }
+
+    public void Heal()
+    {
+        Console.WriteLine("Heal");
+    }
+}
+class Program_44
+{
+    static void Main(string[] args)
+    {
+        IPlayer player = new Player();
+        player.Move();
+        player.Attack();
+        player.Heal();
+    }
+}
