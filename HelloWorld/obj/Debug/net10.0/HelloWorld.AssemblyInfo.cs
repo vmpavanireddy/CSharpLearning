7f63c23eaa6024aa2e601f54a15419a5cf3f1be0
vmpavanireddy <vmpavanireddy@gmail.com>
2026-08-25 00:40:12 +0530

    Abstract Class Partially Implementing Interface

--- HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51a767374f70f0aa76cfbac4674fc3c24897a676")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b3707189bb4437ebe2b95eb1421b05fc194b54c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
