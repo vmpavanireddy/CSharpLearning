bde3cc81b70c2363f57d047ca1a7433dc5331829
vmpavanireddy <vmpavanireddy@gmail.com>
2026-08-24 23:30:29 +0530

    Interface Inheriting Multiple Interfaces

---
+++ HelloWorld/obj/Debug/net10.0/Program_64.cs
@@ -0,0 +1,46 @@
+using System;
+
+interface IMovable
+{
+    void Move();
+}
+
+interface IAttackable
+{
+    void Attack();
+}
+
+interface IPlayer : IMovable, IAttackable
+{
+    void Heal();
+}
+
+class Player : IPlayer
+{
+    public void Move()
+    {
+        Console.WriteLine("Move");
+    }
+
+    public void Attack()
+    {
+        Console.WriteLine("Attack");
+    }
+
+    public void Heal()
+    {
+        Console.WriteLine("Heal");
+    }
+}
+
+class Program_64
+{
+    static void Main()
+    {
+        Player player = new Player();
+
+        player.Move();
+        player.Attack();
+        player.Heal();
+    }
+}
